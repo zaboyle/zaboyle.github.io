3fd502d2909e899d7d1d8b2524986169febefe04
zboyle <zboyle@umich.edu>
2019-08-26 01:57:53 -0400

    implemented typing feature

---
+++ assets/js/default.js
@@ -0,0 +1,27 @@
+function typeWord(message, speed, speed_mult = 1, i = 0) {
+	if (i < message.length) {
+		document.getElementById('typing').innerHTML += message.charAt(i);
+		setTimeout(typeWord, speed, message, speed / speed_mult, speed_mult, ++i);
+	}
+}
+
+function deleteWord(speed, speed_mult = 1, i = document.getElementById('typing').innerHTML.length) {
+	if (i >= 0) {
+		document.getElementById('typing').innerHTML = document.getElementById('typing').innerHTML.slice(0, -1);
+		setTimeout(deleteWord, speed, speed / speed_mult, speed_mult, --i);
+	}
+}
+
+function typeAll() {
+	let slow = 200, med = 150, fast = 100;
+	let speed_up = 1.5, slow_down = 0.8;
+	setTimeout(() => { typeWord('student', med, slow_down); }, 1000);
+	setTimeout(() => { deleteWord(slow, speed_up); }, 3600);
+	setTimeout(() => { typeWord('engineer', fast); }, 4500);
+	setTimeout(() => { deleteWord(med); }, 6000);
+	setTimeout(() => { typeWord('developer', slow, speed_up); }, 7500);
+	setTimeout(() => { deleteWord(med); }, 8500);
+	setTimeout(() => { typeWord('computer scientist', slow); }, 10500);
+}
+
+typeAll();
