5c694ef35c876c05864448cf2a44b9340fb5b4d7
zboyle <zboyle@umich.edu>
2019-08-29 00:08:00 -0400

    added blinking cursor

--- _site/assets/js/default.js
+++ _site/assets/js/default.js
@@ -15,14 +15,14 @@ function deleteWord(speed, speed_mult = 1, i = document.getElementById('typing')
 function typeAll() {
 	let slow = 200, med = 150, fast = 100, very_fast = 50;
 	let speed_up = 1.5, slow_down = 0.9;
-	setTimeout(() => { typeWord('student', med, slow_down); }, 1000);
-	setTimeout(() => { deleteWord(slow, speed_up); }, 3600);
+	setTimeout(() => { typeWord('student', fast, slow_down); }, 1500);
+	setTimeout(() => { deleteWord(med, speed_up); }, 3500);
 	setTimeout(() => { typeWord('engineer', fast); }, 4500);
 	setTimeout(() => { deleteWord(med); }, 6000);
 	setTimeout(() => { typeWord('developer', slow, speed_up); }, 7500);
-	setTimeout(() => { deleteWord(med); }, 9000);
-	setTimeout(() => { typeWord('computer scientist', very_fast, slow_down); }, 11500);
-	setTimeout(() => { showContent(); }, 14000);
+	setTimeout(() => { deleteWord(med, speed_up); }, 9500);
+	setTimeout(() => { typeWord('computer scientist', very_fast, slow_down); }, 11000);
+	setTimeout(() => { showContent(); }, 13500);
 }
 
 function showContent() {
